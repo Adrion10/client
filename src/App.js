6bcb3eaf7db6f4af31d23b4c24a0829a56c8e877
Adrion10 <lacehila@gmail.com>
2022-05-11 21:02:06 +0200

    form post posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "material-ui/core";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
-import divingMemoris from "./images/memorise.jpg";
+import divingMemories from "./images/memorise.jpg";
 const App = () => {
   return (
-    <Container maxwidth="lg">
-      <AppBar position="static" color="primary">
-        <Typography variant="H2" align="center"></Typography>
-        <img src={divingMemoris} alt="memorise" height="60" />
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={divingMemories}
+          alt="icon"
+          height="60"
+        />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
