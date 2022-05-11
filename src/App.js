1be51b33d832853bdc0dbfacaadc771cda43a10c
Adrion10 <lacehila@gmail.com>
2022-05-11 15:32:54 +0200

    create post

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "material-ui/core";
 
+import divingMemoris from "./images/memorise.jpg";
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="primary">
+        <Typography variant="H2" align="center"></Typography>
+        <img src={divingMemoris} alt="memorise" height="60" />
+      </AppBar>
+    </Container>
+  );
 };
 
 export default App;
